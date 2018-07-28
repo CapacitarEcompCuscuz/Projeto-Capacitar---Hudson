6a0644a1f21aed1bf9eb23fa811b98fdfa6d0ca2
Hudson Lopes <hudsonlopes@polijuniorengenharia.com.br>
2018-07-28 14:40:13 -0300

    Projeto finalizado

--- servicos.php
+++ servicos.php
@@ -13,27 +13,33 @@
 
 	<div class="box">
 		<div class="box1">
+			<div class="servico-top">
+				<h2>Serviço 1</h2>
+			</div>
 			
 			<div class="img-box">
-				<img widht="250px" height="200px" src="images/serv1.jpg">
+				<img widht="250px" height="150px" src="images/serv1.jpg">
 			</div>
 			
 			<div class="txt-box1">
-				<h2>Serviço 1</h2>
-				<p>Do reprehenderit laboris ut ullamco aliquip cupidatat pariatur deserunt ullamco et ut officia duis excepteur. Officia veniam enim excepteur magna 
+				
+				<p>Do reprehenderit laboris ut ullamco aliquip cupidatat pariatur deserunt ullamco et ut officia duis excepteur. Officia veniam enim excepteur magna aboris ut ullamco aliquip cupidatat pariatur deserunt ullamco et ut officia duis excepteur. Officia veniam enim excepteur magna 
 				</p>
 			</div>
 			
 		</div>
 
 		<div class="box2">
+			<div class="servico-top">
+				<h2>Serviço 2</h2>
+			</div>
 			
 			<div class="img-box">
-				<img widht="250px" height="200px" src="images/serv1.jpg">
+				<img widht="250px" height="150px" src="images/serv1.jpg">
 			</div>
 			<div class="txt-box1">
-				<h2>Serviço 2</h2>
-				<p>Do reprehenderit laboris ut ullamco aliquip cupidatat pariatur deserunt ullamco et ut officia duis excepteur. Officia veniam enim excepteur magna 
+				
+				<p>DDo reprehenderit laboris ut ullamco aliquip cupidatat pariatur deserunt ullamco et ut officia duis excepteur. Officia veniam enim excepteur magna aboris ut ullamco aliquip cupidatat pariatur deserunt ullamco et ut officia duis excepteur. Officia veniam enim ex
 				</p>
 			</div>
 		</div>
@@ -42,27 +48,30 @@
 
 	<div class="box">
 		<div class="box1">
-			
+			<div class="servico-top">
+				<h2>Serviço 3</h2>
+			</div>
 			<div class="img-box">
-				<img widht="250px" height="200px" src="images/serv1.jpg">
+				<img widht="250px" height="150px" src="images/serv1.jpg">
 			</div>
 			
 			<div class="txt-box1">
-				<h2>Serviço 3</h2>
-				<p>Do reprehenderit laboris ut ullamco aliquip cupidatat pariatur deserunt ullamco et ut officia duis excepteur. Officia veniam enim excepteur magna 
+				
+				<p>Do reprehenderit laboris ut ullamco aliquip cupidatat pariatur deserunt ullamco et ut officia duis excepteur. Officia veniam enim excepteur magna aboris ut ullamco aliquip cupidatat pariatur deserunt ullamco et ut officia duis excepteur. Officia veniam enim excepteur magna 
 				</p>
 			</div>
 			
 		</div>
 
 		<div class="box2">
-			
+			<div class="servico-top">
+				<h2>Serviço 4</h2>
+			</div>
 			<div class="img-box">
-				<img widht="250px" height="200px" src="images/serv1.jpg">
+				<img widht="250px" height="150px" src="images/serv1.jpg">
 			</div>
 			<div class="txt-box1">
-				<h2>Serviço 4</h2>
-				<p>Do reprehenderit laboris ut ullamco aliquip cupidatat pariatur deserunt ullamco et ut officia duis excepteur. Officia veniam enim excepteur magna 
+				<p>Do reprehenderit laboris ut ullamco aliquip cupidatat pariatur deserunt ullamco et ut officia duis excepteur. Officia veniam enim excepteur magna aboris ut ullamco aliquip cupidatat pariatur deserunt ullamco et ut officia duis excepteur. Officia veniam enim excepteur magna 
 				</p>
 			</div>
 		</div>
@@ -71,27 +80,30 @@
 
 	<div class="box-2">
 		<div class="box1">
-			
+			<div class="servico-top">
+				<h2>Serviço 5</h2>
+			</div>
 			<div class="img-box">
-				<img widht="250px" height="200px" src="images/serv1.jpg">
+				<img widht="250px" height="150px" src="images/serv1.jpg">
 			</div>
 			
 			<div class="txt-box1">
-				<h2>Serviço 5</h2>
-				<p>Do reprehenderit laboris ut ullamco aliquip cupidatat pariatur deserunt ullamco et ut officia duis excepteur. Officia veniam enim excepteur magna 
+				<p>Do reprehenderit laboris ut ullamco aliquip cupidatat pariatur deserunt ullamco et ut officia duis excepteur. Officia veniam enim excepteur magna aboris ut ullamco aliquip cupidatat pariatur deserunt ullamco et ut officia duis excepteur. Officia veniam enim excepteur magna 
 				</p>
 			</div>
 			
 		</div>
 
 		<div class="box2">
-			
+			<div class="servico-top">
+				<h2>Serviço 6</h2>
+			</div>
 			<div class="img-box">
-				<img widht="250px" height="200px" src="images/serv1.jpg">
+				<img widht="250px" height="150px" src="images/serv1.jpg">
 			</div>
 			<div class="txt-box1">
-				<h2>Serviço 6</h2>
-				<p>Do reprehenderit laboris ut ullamco aliquip cupidatat pariatur deserunt ullamco et ut officia duis excepteur. Officia veniam enim excepteur magna 
+				
+				<p>Do reprehenderit laboris ut ullamco aliquip cupidatat pariatur deserunt ullamco et ut officia duis excepteur. Officia veniam enim excepteur magna aboris ut ullamco aliquip cupidatat pariatur deserunt ullamco et ut officia duis excepteur. Officia veniam enim excepteur magna 
 				</p>
 			</div>
 		</div>
